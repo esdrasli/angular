238309a51edaa29936300d1153688faa49e3df61
esdrasli <42820970+esdrasli@users.noreply.github.com>
2021-02-23 01:53:06 -0300

    lazy loading

--- src/app/extrato/extrato-routing.module.ts
+++ src/app/extrato/extrato-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExtratoComponent } from './extrato.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: ExtratoComponent,
+}]
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
